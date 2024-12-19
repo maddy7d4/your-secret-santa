d97e15b618cead43a6610e106372dbe8466b9ab2
maddy7d4 <madhavanuser@gmail.com>
2024-12-19 21:46:13 +0530

    edited copyrights

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
       <footer className="py-4 text-center text-sm text-gray-600">
-          &copy; 2024 Madhavan. All rights reserved.
+          &copy; 2024 Madhavan.
       </footer>
   )
 }
